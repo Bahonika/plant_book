5c840a8789336bcfee9d10cbe6ad9cf7561d900c
Bahonika <bahonika1@mail.ru>
2022-04-10 23:32:03 +0300

    merged

--- lib/data/repositories/abstract/api.dart
+++ lib/data/repositories/abstract/api.dart
@@ -1,10 +1,10 @@
 abstract class Api {
 
-  // static const String siteRoot = "projects.masu.edu.ru";
-  // static const String apiRoot = "/herbarium/api/";
-
-  static const String siteRoot = "localhost:8000";
+  static const String siteRoot = "projects.masu.edu.ru";
   static const String apiRoot = "/herbarium/api/";
+
+  // static const String siteRoot = "localhost:8000";
+  // static const String apiRoot = "/herbarium/api/";
   String get apiEndpoint;
 
   String apiPath() => apiRoot+apiEndpoint;
